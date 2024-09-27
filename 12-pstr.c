77906f080f0fd6aedfdae4d3a3b71389d5c0c344
Abdul-Mumin Awinaba <awinabaab@gmail.com>
2024-09-27 12:31:32 +0000

    Add 12-pstr.c

---
+++ 12-pstr.c
@@ -0,0 +1,23 @@
+#include "monty.h"
+
+/**
+ * pstr_op - Prints the value at the top of the stack followed by a new line
+ * @stack: Stack
+ * @line_number: Line number of bytecode operation
+ */
+
+void pstr_op(stack_t **stack, unsigned int line_number)
+{
+	stack_t *top = *stack;
+
+	(void)line_number;
+
+	if (!stack || !*stack)
+		printf("\n");
+	while (top && top->n != 0 && isascii(top->n))
+	{
+		printf("%c", top->n);
+		top = top->next;
+	}
+	printf("\n");
+}
--- get_op_func.c
+++ get_op_func.c
@@ -23,6 +23,7 @@ void (*get_op_func(char *op))(stack_t **stack, unsigned int line_number)
 		{"mul", mul_op},
 		{"mod", mod_op},
 		{"pchar", pchar_op},
+		{"pstr", pstr_op},
 		{NULL, NULL}
 	};
 
