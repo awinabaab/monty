00e384c19fcaa58ad602355e0dc589a36b783b98
Abdul-Mumin Awinaba <awinabaab@gmail.com>
2024-09-27 12:11:58 +0000

    Rename

---
+++ 11-pchar.c
@@ -0,0 +1,29 @@
+#include "monty.h"
+
+/**
+ * pchar_op - Prints the value at the top of the stack followed by a new line
+ * @stack: Stack
+ * @line_number: Line number of bytecode operation
+ */
+
+void pchar_op(stack_t **stack, unsigned int line_number)
+{
+	if (!*stack)
+	{
+		fflush(stdout);
+		dprintf(STDERR_FILENO,
+				"L%d: can't pchar, stack empty\n",
+				line_number);
+		free_on_failure();
+		exit(EXIT_FAILURE);
+	}
+	if (!isascii((*stack)->n))
+	{
+		dprintf(STDERR_FILENO,
+				"L%d: can't pchar, value out of range\n",
+				line_number);
+		free_on_failure();
+		exit(EXIT_FAILURE);
+	}
+	printf("%c\n", (*stack)->n);
+}
