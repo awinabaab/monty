e533709145a741b7774d78e40ca32985c13e61d5
Abdul-Mumin Awinaba <awinabaab@gmail.com>
2024-09-27 06:48:26 +0000

    Fix: handle NULL case for command.op_arg

--- 0-push_pall.c
+++ 0-push_pall.c
@@ -11,7 +11,8 @@ void push_op(stack_t **stack, unsigned int line_number)
 	stack_t *top = *stack;
 	stack_t *new_element = NULL;
 
-	if (command.op_arg && check_digit(command.op_arg) == 0)
+	if ((command.op_arg && check_digit(command.op_arg) == 0) ||
+			!command.op_arg)
 	{
 		fprintf(stderr, "L%d: usage: push integer\n", line_number);
 		free_on_failure();
